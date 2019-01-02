d38b0569ff9d16745cf5b9cadce7e2cd1bf6b461
ironxing <liuxing77777@gmail.com>
2019-01-02 13:02:11 +0100

    changedConnectionString

--- DatingSite/Global.asax.cs
+++ DatingSite/Global.asax.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,6 +13,8 @@ namespace DatingSite
     {
         protected void Application_Start()
         {
+            //Database.SetInitializer(new DropCreateDatabaseAlways<>());
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
